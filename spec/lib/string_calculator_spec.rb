1b9e37d525c16bd3b3f84beeb947507fb960ead5
Vikas Dubey <vikasrordev@gmail.com>
2024-12-23 19:39:10 +0530

    Case2: Test fails because the add method doesn't return 1

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -4,9 +4,14 @@ require 'rails_helper'
 require 'string_calculator'
 
 RSpec.describe StringCalculator do
+  subject { described_class.new }
   describe '#add' do
     it 'output should be 0 for an empty string' do
-      expect(described_class.new.add("")).to eq(0)
+      expect(subject.add("")).to eq(0)
+    end
+
+    it 'output the number itself if only one input is given' do
+      expect(subject.add("1")).to eq(1)
     end
   end
 end
