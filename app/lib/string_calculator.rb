ebfe34cc01c934332cc13035f36b9db52d9adea7
Vikas Dubey <vikasrordev@gmail.com>
2024-12-23 21:15:05 +0530

    Case7: Test pass because the add method return 3

--- app/lib/string_calculator.rb
+++ app/lib/string_calculator.rb
@@ -4,6 +4,14 @@ class StringCalculator
   def add(input)
     return 0 if input.empty?
 
-    input.split(/[\n,]/).map(&:to_i).sum
+    delimiter = /[\n,]/
+    if input.start_with?("//")
+      delimiter_string, input = input.split("\n")
+      delimiter = /#{delimiter_string.split("//").last}/
+    else
+      delimiter = /[\n,]/
+    end
+
+    input.split(delimiter).map(&:to_i).sum
   end
 end
